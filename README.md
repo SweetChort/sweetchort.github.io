95b4fb08aec8fa6d4bab7c0e806e2d90672cf8d5
abartier <antoine@hestiasolution.com>
2020-01-13 14:49:02 -0500

    init from panda archive

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# sweetchort.github.io
+# Le site des commandes de la loutre est opensource!
+
+Pensez à faire coucou sur son stream! 
+
---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+// Sound player
+const soundContainers = document.querySelectorAll('.sound-container');
+
+soundContainers.forEach(element => {
+  element.addEventListener('click', function() {
+    element.classList.add('pushed');
+    var arrayOfSounds = element.querySelectorAll('audio');
+    var rand = arrayOfSounds[Math.floor(Math.random() * arrayOfSounds.length)];
+    rand.play();
+    setTimeout(function(){
+      element.classList.remove('pushed');
+    }, 500);
+  });
+});
+
+
+var options = {
+  strings: [
+    "Le panda ne vaut rien",
+    "Viendez sur ma chaine qu'elle est mieux",
+  ],
+  typeSpeed: 50,
+  backSpeed: 20,
+  shuffle: false,
+  showCursor: false
+};
+
+// random sentences
+var typed = new Typed('.random-sentences', options);
+
+
+// clipboard.js
+var clipboard = new ClipboardJS('.uk-card');
+
+// clipboard.on('success', function(e) {
+//     console.info('Action:', e.action);
+//     console.info('Text:', e.text);
+//     console.info('Trigger:', e.trigger);
+
+//     e.clearSelection();
+// });
